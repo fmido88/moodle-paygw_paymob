d4da6be80548fddf00e58041b0c04a536e8fcba4
Mo Farouk <122031508+fmido88@users.noreply.github.com>
2023-11-25 17:09:11 +0200

    Change version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'paygw_paymob';
-$plugin->release = '1.0';
-$plugin->version = 2023052001;
-$plugin->requires = 2022112800;
-$plugin->maturity = MATURITY_BETA;
+$plugin->release   = '2.0';
+$plugin->version   = 2023112517;
+$plugin->requires  = 2022112800;
+$plugin->maturity  = MATURITY_BETA;
